76ace12796ce97b23d5fd16b57e9603316985607
palavithakur <tanvithakur046@gmail.com>
2024-10-24 14:59:51 +0530

    Dynamic Card data

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,27 +1,27 @@
 import RecipeCard from "../components/RecipeCard";
+import { getRandomColor } from "../lib/utils";
 
-const Favorites = () => {
-  const fav= true;
-  return (
-    <div className='bg-[#faf9fb] flex-1 p-10 min-h-screen'>
-      <div className="max-w-screen-lg mx-auto">
-        <p className="font-bold text-3xl md:text-5xl my-4">  My Favorites</p>
-          
-          {!fav && ( 
-            <div className="h-[80vh] flex flex-col items-center gap-4">
-              <img src='/404.svg' className="'h-3/4 " alt="404 svg"/>
-            </div>
-          )}
+const FavoritesPage = () => {
+	const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
-          {fav && (
-            <div className='grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-              <RecipeCard/>
-            </div>
-          )}
-      
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className='bg-[#faf9fb] flex-1 p-10 min-h-screen'>
+			<div className='max-w-screen-lg mx-auto'>
+				<p className='font-bold text-3xl md:text-5xl my-4'>My Favorites</p>
 
-export default Favorites
+				{favorites.length === 0 && (
+					<div className='h-[80vh] flex flex-col items-center gap-4'>
+						<img src='/404.svg' className='h-3/4' alt='404 svg' />
+					</div>
+				)}
+
+				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+					{favorites.map((recipe) => (
+						<RecipeCard key={recipe.label} recipe={recipe} {...getRandomColor()} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
+export default FavoritesPage;
